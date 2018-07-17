7d9a040ef8cef8582aeebe292fe8632a075ecbe5
Evgeni7 <40259589+Evgeni7@users.noreply.github.com>
2018-07-17 16:41:58 +0300

    Gets machine name and time

---
+++ dotnet/DateController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace date
+{
+    [Route("api/date")]
+    public class HelthCheckController : Controller
+    {
+        /// <summary>
+        /// Endpoint used for health checks
+        /// </summary>
+        /// <returns>Returns the name of the controller and the time of the call.</returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {Environment.MachineName.ToString() + " - " + DateTime.UtcNow.ToString()};
+          
+        
+            }
+        
+
+
+
+    }
+}
